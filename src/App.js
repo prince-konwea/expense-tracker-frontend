ae530e5ae84d2bf45c40b6a292fb35dea224cea5
prince-konwea <kokodecontee123@gmail.com>
2023-07-10 10:54:49 +0100

    creating mainLayouts in globalstyles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-
+import styled from "styled-components"
 import './App.css';
+import { MainLayout } from "./styles/layout";
+import bg from "./image/bg.png"
 
 function App() {
   return (
-    <div className="App">
+    <AppStyled bg={bg} className="App">
+    <MainLayout>
+      <h1>Gluxx</h1>
+    </MainLayout>
 
-    </div>
+    </AppStyled>
   );
 }
+
+
+
+const AppStyled = styled.div`
+ height: 100vh;
+ background-image: url({props => prop.bg})
+ position: relative;
+
+`;
 
 export default App;
--- src/styles/globalstyles.js
+++ src/styles/globalstyles.js
@@ -25,7 +25,9 @@ export const Globalstyle = createGlobalStyle`
  body{
     font-family: 'IBM Plex Serif', serif;
 font-family: 'Nunito', sans-serif;
-background-color: red;
+ font-size: clamp (1rem, 1.5vw, 1.2rem);
+ overflow: hidden;
+ color: rgba(34, 34, 34, .6)
  }
 
 `
